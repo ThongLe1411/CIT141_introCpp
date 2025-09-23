f43a5a285783a6ef24f67cf1892ee761ca8bd46a
Kyle <104819955+ThongLe1411@users.noreply.github.com>
2025-09-22 21:56:09 -0500

    Update isPalidrome.cpp
    Add Version 2 focused on the idea of comparing from inside to outside. 
    
    Unfortunately, ver2 seems only work with even length.

--- isPalidrome.cpp
+++ isPalidrome.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-bool isPalindrome();
+bool isPalindrome(string str); //function declaration
 int main() {
-    // Write your main here
     string str; 
     cout << "Enter your string: "; 
-    cin >> str; 
+    cin >> str; //input
 
     if(isPalindrome(str) == 1)  
-        cout << isPalindrome(str) << " is a palindrome";
+        cout << str << " is a palindrome";
     else
-        cout << isPalindrome(str) << " is not a palindrome";
+        cout << str << " is not a palindrome";
+    //output
     return 0;
 }
 bool isPalindrome(string str) 
@@ -20,10 +20,17 @@ bool isPalindrome(string str)
     int length = str.length(); 
     char ch1, ch2;
     for (int i = 0; i < length / 2; i++) {
-    // str[i] = std::tolower(str[i]);
+        
+    //Version 1: compare from outside to inside (work with odd and even length)
     ch1 = tolower(str[i]);
     ch2 = tolower( str[length - 1 - i]);
-    if (ch1 != ch2) {
+    /* 
+    ch1 = toupper(str[length / 2 - 1 - i]);
+    ch2 = toupper(str[length / 2 + i]);
+    Version 2: compare from inside to outside 
+    I dont know but it seems like ver2 only works with words whose even length
+    */
+    if (ch1 != ch2) { //compare two symmetric words from outside to inside
          return false;
     } // if    
   } // for loop
