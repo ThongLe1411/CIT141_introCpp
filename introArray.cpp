4330600c6fd4ee82607c33b8b2f6b8d3e68c7272
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2025-09-24 11:31:05 -0500

    Create introArray.cpp

---
+++ introArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // when declaring an array, you need to specify the number of elements in the array
+  string cars[] = {"volvo", "bmw", "Tesla", "Toyota"};
+  cars[0] = "Ford"; // change the component;
+
+  int n = sizeof(cars) / sizeof(cars[0]); // sizeof(cars[0]);
+  // sizeof return the number of bits
+  cout << n << endl;
+
+  int arrayLength = sizeof(cars); // Get the length of an array
+
+  for (int i = 0; i <= arrayLength; i++) // Listing array elements
+  {
+    cout << cars[i] << "\n";
+
+  } // expect output : ford \n bmw \n Tesla \n Toyota
+}
