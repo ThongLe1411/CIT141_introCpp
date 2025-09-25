c387bd873ff750797817ef642ce78d12823cd88d
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2025-09-25 12:17:41 -0500

    Update calculator.cpp
    More detailed and readable code.

--- calculator.cpp
+++ calculator.cpp
@@ -1,71 +1,88 @@
 #include <iostream>
-using namespace std;
-const double PI = 3.14159;
-double rectangle(double l, double w);
-    double circle(double r);
-    double cylinder(double bR, double h);
 #include <iomanip>
+using namespace std;
+// declare functions for calculator program;
+double circleArea(double radius);
+double rectangleArea(double lenght, double width);
+double cylinderVolume(double radius, double height);
+double cylinderArea(double radius, double height);
+
+const double PI = 3.14159; // constant are usually capitalized
+
 int main()
 {
-    double r;
-    double h;
-    int choice;
-    double l, w; 
-    cout << fixed << showpoint << setprecision(2) << endl;
-    cout << "This program can calculate the area of a rectangle, "
-        << "the area of a circle, or volume of a cylinder." << endl;
-    cout << "To run the program enter: " << endl;
-    cout << "1: To find the area of rectangle." << endl;
-    cout << "2: To find the area of a circle." << endl;
-    cout << "3: To find the volume of a cylinder." << endl;
-    cout << "-1: To terminate the program." << endl;
-    cin >> choice;
-    cout << endl;
-    while (choice != -1)
+  // declare local variable 
+  double radius, height;
+  double length, width;
+  int choice;
+
+  // round to 3 decimal places
+  cout << fixed << showpoint << setprecision(3);
+  cout << "******Welcome to calculator******* \n";
+  cout << "1. Calculate the Area of a circle \n";
+  cout << "2. Calculate the Area of a rectangle \n";
+  cout << "3. Calculate the Volume of a cylinder \n";
+  cout << "4. Calculate the Area of a cylinder \n";
+
+  cout << "Enter 1, 2, 3, 4 to perform your calculation\n "
+       << "Or enter -1 to terminate the program: ";
+  // program input
+  cin >> choice;
+
+  while (choice != -1) // program output
+  {
+    switch (choice)
     {
-        switch (choice){
-        case 1: 
-            cout << "Enter the length and the width "
-                 << "of the rectangle: ";
-            cin >> l >> w;
-            
-              cout << "Area = " << rectangle(l, w)<<  endl;
-
-
-            break;
-        case 2:
-            cout << "Enter the radius of the circle: ";
-            cin >> r;
-            cout << endl;
-            cout << "Area = " << circle(r)
-                 << endl;
-
-            break;
-        case 3:
-            cout << "Enter the radius of the base and the "
-                 << "height of the cylinder: ";
-            cin >> r >> h;
-            cout << endl;
-            cout << "Volume = " << cylinder(r, h) 
-                 << endl;
-            break;
-        default:
-            cout << "Invalid choice!" << endl;
-            cin >> choice;
-        }
-        cin >> choice;
+    case 1:
+      cout << "Enter the radius of your circle: ";
+      cin >> radius;
+      cout << "The circle area is: " << circleArea(radius) << " square units";
+      break;
+    case 2:
+      cout << "Enter the length of your rectangle: ";
+      cin >> length;
+      cout << "\n Enter the width of your rectangle: ";
+      cin >> width;
+      cout << "The rectangle area is " << rectangleArea(length, width) << " square units";
+      break;
+    case 3:
+      cout << "Enter the radius of your cylinder: ";
+      cin >> radius;
+      cout << "\n Enter the height of your cylinder: ";
+      cin >> height;
+      cout << "The Volume of the cylinder is: " << cylinderVolume(radius, height);
+      break;
+    case 4:
+      cout << "Enter the radius of your cylinder: ";
+      cin >> radius;
+      cout << "\n Enter the height of your cylinder: ";
+      cin >> height;
+      cout << "The area of the cylinder is: " << cylinderArea(radius, height);
+      break;
+    default:
+      cout << "Your input is not match with our program \n"
+           << "Please enter again or -1 to terminate the program: ";
+      cin >> choice;
+      break;
     }
-    return 0;
- }
-double rectangle(double l, double w)
-{
-    return l * w;
+    cin >> choice;
+  }
+  return 0;
 }
-double circle(double r)
+//functions' definition:
+double circleArea(double radius)
 {
-    return PI * r * r;
+  return radius * radius * PI;
 }
-double cylinder(double bR, double h)
+double rectangleArea(double length, double width)
 {
-    return PI * bR * bR * h;
+  return length * width;
+}
+double cylinderVolume(double radius, double height)
+{
+  return radius * radius * PI * height;
+}
+double cylinderArea(double radius, double height)
+{
+  return 2 * PI * radius * radius + 2 * PI * radius * height;
 }
