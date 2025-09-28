40841a9e1c740562827adb2ad5082f0bedf1492a
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2025-09-27 20:13:31 -0500

    Update introArray.cpp

--- introArray.cpp
+++ introArray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
   // when declaring an array, you need to specify the number of elements in the array
-  string cars[] = {"volvo", "bmw", "Tesla", "Toyota"};
+  string cars[4] = {"volvo", "bmw", "Tesla", "Toyota"};
   cars[0] = "Ford"; // change the component;
 
   int n = sizeof(cars) / sizeof(cars[0]); // sizeof(cars[0]);
@@ -16,6 +16,5 @@ int main()
   for (int i = 0; i <= arrayLength; i++) // Listing array elements
   {
     cout << cars[i] << "\n";
-
   } // expect output : ford \n bmw \n Tesla \n Toyota
 }
